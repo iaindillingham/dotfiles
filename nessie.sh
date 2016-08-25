b64680cb845cbd44fc7f7cc7214160396be17110
Iain Dillingham <iain.dillingham@maplecroft.com>
2016-08-25 09:39:14 +0100

    No more conf file for Nessie

--- nessie.sh
+++ nessie.sh
@@ -1,3 +1,65 @@
 #!/bin/sh
 
-tmux -f '/home/iain/Code/dotfiles/nessie.conf' attach
+# Preliminaries
+
+ANNOTATOR_STORE_HOME='/home/iain/Code/annotator-store'
+
+NESSIE_HOME='/home/iain/Code/nessie'
+
+tmux new-session -s nessie -n annotator -d
+
+# Annotator Store
+
+tmux send-keys -t nessie "cd ${ANNOTATOR_STORE_HOME}" C-m
+
+tmux send-keys -t nessie "vex annotator-store python run.py" C-m
+
+# Django
+
+tmux new-window -t nessie -n django
+
+tmux send-keys -t nessie "cd ${NESSIE_HOME}" C-m
+
+tmux send-keys -t nessie "vex nessie python manage.py runserver 127.0.0.1:8000" C-m
+
+# Tox
+
+tmux new-window -t nessie -n tox
+
+tmux send-keys -t nessie "cd ${NESSIE_HOME}" C-m
+
+tmux send-keys -t nessie "vex nessie tox" C-m
+
+# Editor
+
+tmux new-window -t nessie -n editor
+
+tmux send-keys -t nessie "cd ${NESSIE_HOME}" C-m
+
+tmux send-keys -t nessie "vim" C-m
+
+# Git
+
+tmux new-window -t nessie -n git
+
+tmux send-keys -t nessie "cd ${NESSIE_HOME}" C-m
+
+tmux send-keys -t nessie "git branch" C-m
+
+tmux split-window -t nessie -h
+
+tmux send-keys -t nessie "cd ${NESSIE_HOME}" C-m
+
+# Shell
+
+tmux new-window -t nessie -n shell
+
+tmux send-keys -t nessie "cd ${NESSIE_HOME}" C-m
+
+tmux send-keys -t nessie "vex nessie python manage.py shell" C-m
+
+# Let's go!
+
+tmux select-window -t nessie:git
+
+tmux attach-session -t nessie
