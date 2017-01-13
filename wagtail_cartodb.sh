e2eb4c26bbca709c7954d74a0c994ea815e338cc
Iain Dillingham <iain.dillingham@maplecroft.com>
2017-01-13 15:14:17 +0000

    Add wagtail cartodb tmux script

---
+++ wagtail_cartodb.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# Preliminaries
+
+PROJECT_HOME='~/Code/wagtail_cartodb'
+
+PROJECT_NAME=`basename ${PROJECT_HOME}`
+
+tmux -2 new-session -s ${PROJECT_NAME} -n tox -d
+
+# Tox
+
+tmux send-keys -t ${PROJECT_NAME} "cd ${PROJECT_HOME}" C-m
+
+tmux send-keys -t ${PROJECT_NAME} "vex ${PROJECT_NAME} tox" C-m
+
+# Editor
+
+tmux new-window -t ${PROJECT_NAME} -n editor
+
+tmux send-keys -t ${PROJECT_NAME} "cd ${PROJECT_HOME}" C-m
+
+tmux send-keys -t ${PROJECT_NAME} "vim" C-m
+
+# Git
+
+tmux new-window -t ${PROJECT_NAME} -n git
+
+tmux send-keys -t ${PROJECT_NAME} "cd ${PROJECT_HOME}" C-m
+
+tmux send-keys -t ${PROJECT_NAME} "git branch" C-m
+
+tmux split-window -t ${PROJECT_NAME} -h
+
+tmux send-keys -t ${PROJECT_NAME} "cd ${PROJECT_HOME}" C-m
+
+# Let's go!
+
+tmux -2 attach-session -t ${PROJECT_NAME}
