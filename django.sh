dc1f2e30372a5fe89dbae7c231e0240fdbc34f05
Iain Dillingham <iain.dillingham@maplecroft.com>
2017-01-27 19:19:41 +0000

    Reorder windows

--- django.sh
+++ django.sh
@@ -11,17 +11,9 @@ fi
 
 PROJECT_NAME=`basename ${PROJECT_HOME}`
 
-tmux -2 new-session -s ${PROJECT_NAME} -n tox -d
-
-# Tox
-
-tmux send-keys -t ${PROJECT_NAME} "cd ${PROJECT_HOME}" C-m
-
-tmux send-keys -t ${PROJECT_NAME} "vex ${PROJECT_NAME} tox" C-m
+tmux -2 new-session -s ${PROJECT_NAME} -n editor -d
 
 # Editor
-
-tmux new-window -t ${PROJECT_NAME} -n editor
 
 tmux send-keys -t ${PROJECT_NAME} "cd ${PROJECT_HOME}" C-m
 
@@ -39,6 +31,16 @@ tmux split-window -t ${PROJECT_NAME} -h
 
 tmux send-keys -t ${PROJECT_NAME} "cd ${PROJECT_HOME}" C-m
 
+# Tox
+
+tmux new-window -t ${PROJECT_NAME} -n tox
+
+tmux send-keys -t ${PROJECT_NAME} "cd ${PROJECT_HOME}" C-m
+
+tmux send-keys -t ${PROJECT_NAME} "vex ${PROJECT_NAME} tox" C-m
+
 # Let's go!
+
+tmux select-window -t ${PROJECT_NAME}:git
 
 tmux -2 attach-session -t ${PROJECT_NAME}
