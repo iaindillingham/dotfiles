5e10a4eb131484a0dcba9bf4997c2d6818d1f2ce
Iain Dillingham <iain.dillingham@maplecroft.com>
2017-07-22 17:23:03 +0100

    Add HipChat

--- provision.sh
+++ provision.sh
@@ -33,3 +33,9 @@ ln -fsv "$DOTFILES_DIR/vimrc" "$HOME/.vimrc"
 # Vundle
 git clone -v https://github.com/VundleVim/Vundle.vim.git "$HOME/.vim/bundle/Vundle.vim"
 vim +PluginInstall +qall
+
+# HipChat
+sudo sh -c 'echo "deb https://atlassian.artifactoryonline.com/atlassian/hipchat-apt-client $(lsb_release -c -s) main" > /etc/apt/sources.list.d/atlassian-hipchat4.list'
+wget -O - https://atlassian.artifactoryonline.com/atlassian/api/gpg/key/public | sudo apt-key add -
+sudo apt update
+sudo apt --yes install hipchat4
