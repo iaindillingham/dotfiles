b4925532d07dd0d38ef7eff1acd522cedba4be49
Iain Dillingham <iain.dillingham@maplecroft.com>
2017-08-13 18:07:56 +0100

    Rename PROJECT_HOME to PROJECT_ROOT_DIR

--- django.sh
+++ django.sh
@@ -5,19 +5,19 @@
 # If the first argument is either the empty string or not a directory,
 # then exit.
 if [ ! -n "$1" ] || [ ! -d "$1" ]; then
-    echo "$0 PROJECT_HOME"
+    echo "$0 PROJECT_ROOT_DIR"
     exit 1
 fi
 
-PROJECT_HOME=$1
+PROJECT_ROOT_DIR=$1
 
-PROJECT_NAME=`basename ${PROJECT_HOME}`
+PROJECT_NAME=`basename ${PROJECT_ROOT_DIR}`
 
 tmux -2 new-session -s ${PROJECT_NAME} -n editor -d
 
 # Editor
 
-tmux send-keys -t ${PROJECT_NAME} "cd ${PROJECT_HOME}" C-m
+tmux send-keys -t ${PROJECT_NAME} "cd ${PROJECT_ROOT_DIR}" C-m
 
 tmux send-keys -t ${PROJECT_NAME} "vim" C-m
 
@@ -25,29 +25,29 @@ tmux send-keys -t ${PROJECT_NAME} "vim" C-m
 
 tmux new-window -t ${PROJECT_NAME} -n git
 
-tmux send-keys -t ${PROJECT_NAME} "cd ${PROJECT_HOME}" C-m
+tmux send-keys -t ${PROJECT_NAME} "cd ${PROJECT_ROOT_DIR}" C-m
 
 tmux send-keys -t ${PROJECT_NAME} "git branch" C-m
 
 tmux split-window -t ${PROJECT_NAME} -h
 
-tmux send-keys -t ${PROJECT_NAME} "cd ${PROJECT_HOME}" C-m
+tmux send-keys -t ${PROJECT_NAME} "cd ${PROJECT_ROOT_DIR}" C-m
 
 # Tox
 
 tmux new-window -t ${PROJECT_NAME} -n tox
 
-tmux send-keys -t ${PROJECT_NAME} "cd ${PROJECT_HOME}" C-m
+tmux send-keys -t ${PROJECT_NAME} "cd ${PROJECT_ROOT_DIR}" C-m
 
 tmux send-keys -t ${PROJECT_NAME} "vex ${PROJECT_NAME} tox" C-m
 
-if [ -e "${PROJECT_HOME}/manage.py" ]; then
+if [ -e "${PROJECT_ROOT_DIR}/manage.py" ]; then
 
     # Shell
 
     tmux new-window -t ${PROJECT_NAME} -n shell
 
-    tmux send-keys -t ${PROJECT_NAME} "cd ${PROJECT_HOME}" C-m
+    tmux send-keys -t ${PROJECT_NAME} "cd ${PROJECT_ROOT_DIR}" C-m
 
     tmux send-keys -t ${PROJECT_NAME} "vex ${PROJECT_NAME} python manage.py shell" C-m
 
@@ -55,7 +55,7 @@ if [ -e "${PROJECT_HOME}/manage.py" ]; then
 
     tmux new-window -t ${PROJECT_NAME} -n django
 
-    tmux send-keys -t ${PROJECT_NAME} "cd ${PROJECT_HOME}" C-m
+    tmux send-keys -t ${PROJECT_NAME} "cd ${PROJECT_ROOT_DIR}" C-m
 
     tmux send-keys -t ${PROJECT_NAME} "vex ${PROJECT_NAME} python manage.py runserver" C-m
 
